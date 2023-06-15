d7cd659cfe259a4fb72f52d62f2cbbdc0abb7cee
Everton Sales <everton.goncalves92@gmail.com>
2023-06-14 23:13:01 -0300

    project created

---
+++ src/test/java/com/tomsales/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomsales.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
